aa187414cfd1e2b938c1276702866bcdffdd29dc
lby132 <quddbscjswow@naver.com>
2024-01-11 10:45:18 +0900

    test

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllBySellingStatusIn(List<ProductSellingType> sellingTypes);
+
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
 }
